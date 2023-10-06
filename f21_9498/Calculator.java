e853bf9629216d7c5cfa383b9eacf28bd05426a3
HafizMAhmad17 <f219498@cfd.nu.edu.pk>
2023-10-06 21:07:57 +0500

    Added Calcultor class

---
+++ f21_9498/Calculator.java
@@ -0,0 +1,14 @@
+package f21_9498;
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x,int y) {
+		return x-y;
+	}
+}
