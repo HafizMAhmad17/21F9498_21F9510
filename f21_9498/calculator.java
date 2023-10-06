350f3109fa84405757c04a3991f460931e575dc1
HafizMAhmad17 <f219498@cfd.nu.edu.pk>
2023-10-06 21:18:33 +0500

    Added Calculator Class with Addition and Substraction

---
+++ f21_9498/calculator.java
@@ -0,0 +1,14 @@
+package f21_9498;
+
+public class calculator {
+
+	public calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	public int add(int x, int y) {
+		return x+y;
+	}
+	public int sub(int x,int y) {
+		return x-y;
+	}
+}
